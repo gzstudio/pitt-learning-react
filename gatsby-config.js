9d90cba82f6c7741aa7c2924ff0ea06ddbd57429
YR <yui.rasananda@gmail.com>
2018-08-27 22:42:48 +1000

    commit project

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,18 @@
 module.exports = {
   siteMetadata: {
     title: 'Pitt + React',
+    description: 'Pitt is learning how to build his first site using React',
+    keywords: 'react, learning react'
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+
+  plugins: [
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: `pfd9jwnkwxdd`,
+        accessToken: `5509a8de6d76ff148d06fe5b4bd8fe246761ed7ec130221333dfc3c4dc57dc56`,
+      },
+    },
+    `gatsby-plugin-react-helmet`
+  ],
 }
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,30 +1,25 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
-
 import Header from '../components/header'
 import './index.css'
+import Footer from '../components/Footer';
+
 
 const Layout = ({ children, data }) => (
   <div>
-    <Helmet
+  <Helmet
       title={data.site.siteMetadata.title}
       meta={[
-        { name: 'description', content: 'Sample' },
-        { name: 'keywords', content: 'sample, something' },
+        { name: 'description', content: data.site.siteMetadata.description },
+	      { name: 'keywords', content: data.site.siteMetadata.keywords },
       ]}
     />
-    <Header siteTitle={data.site.siteMetadata.title} />
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
-      }}
-    >
-      {children()}
-    </div>
+    <Header/>
+    {children()}
+    <Footer data={data}>
+      Backgrounds made in Cinema 4D, iOS app in Swift, Site in React. <a hfref="mailto:pitt.rasananda@gmail.com"> Email us</a> to ask anything. © 2018
+    </Footer>
   </div>
 )
 
@@ -39,6 +34,17 @@ export const query = graphql`
     site {
       siteMetadata {
         title
+        description
+        keywords
+      }
+    }
+    allContentfulLink(sort: { fields: [createdAt], order: ASC }) {
+      edges {
+        node {
+          title
+          url
+          createdAt
+        }
       }
     }
   }
